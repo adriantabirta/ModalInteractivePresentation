9915993a314023b56a5615f471cf96cab6147d04
Nr .meu <adriantabirta@adrians-macbook-pro.local>
2018-03-02 13:18:18 +0200

    Initial Commit

---
+++ ModalInteractivePresentation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ModalInteractivePresentation
+//
+//  Created by Adrian Tabirta on 02.03.2018.
+//  Copyright © 2018 Adrian Tabirta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
